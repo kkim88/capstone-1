be1e5d668ebdbce0241796784d2775c710b1c920
Katherine Kim <katherinekim88@gmail.com>
2020-06-10 22:25:21 -0500

    Added router

--- inventory-management/src/App.js
+++ inventory-management/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from './Components/Search'
+import ShoppingCart from './Components/Cart'
 import './App.css';
+import { Link, Route } from 'react-router-dom';
 
 function App() {
 
+  const [cart, setCart] = useState([]);
+  const [items, setItems] = useState([]);
+
   return (
     <div className="App">
+      <img src='/shopping-cart-black-shape.png' width="100px" height="100px"></img>
       <h1>Let's Shop With React</h1>
+      <nav>
+        <Link to="/cart">Show Cart</Link>
+      </nav>
+      <Route path="/cart">
+        <ShoppingCart />
+      </Route>
+      <p></p>
       <Search />
     </div>
   );
