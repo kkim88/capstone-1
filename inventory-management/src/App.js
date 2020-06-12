0653c6552ab99bfad12304dbb1fa5ec35e38a173
Katherine Kim <katherinekim88@gmail.com>
2020-06-12 01:14:38 -0500

    More fix attempts

--- inventory-management/src/App.js
+++ inventory-management/src/App.js
@@ -8,27 +8,22 @@ import data from './Components/data.json';
 function App() {
 
   const [cart, setCart] = useState([]);
-  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    setItems(data);
-  }, []);
-
-  useEffect(() => {
-    console.log("Items in <App/>", items);
+    // console.log("Items in <App/>", items);
     console.log("cart in <App/>", cart);
   }, []);
 
   return (
     <div className="App">
-      <img src='/shopping-cart-black-shape.png' width="100px" height="100px"></img>
-      <h1>Let's Shop With React</h1>
-      <nav>
-        <Link to="/cart">Show Cart</Link>
-        <Link to="/search" />
-      </nav>
-      <Route exact path="/search">
-        <Search cart={cart} setCart={setCart} items={items} setItems={setItems} />
+      <header>
+        <h1>Odds and Ends and Miscellaney</h1>
+        <nav>
+          <Link to="/cart"><img src='/shopping-cart-black-shape.png' width="18px" height="18px"></img>Show Cart</Link>
+        </nav>
+      </header>
+      <Route exact path="/search" >
+        <Search cart={cart} setCart={setCart} />
       </Route>
       <Route exact path="/cart">
         <ShoppingCart cart={cart} setCart={setCart} />
