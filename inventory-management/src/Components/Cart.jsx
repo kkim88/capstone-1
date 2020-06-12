dd851aa4fe8ac2d31672699b6c461f727782f0ca
Katherine Kim <katherinekim88@gmail.com>
2020-06-12 01:32:18 -0500

    added comments

--- inventory-management/src/Components/Cart.jsx
+++ inventory-management/src/Components/Cart.jsx
@@ -3,17 +3,24 @@ import { Link } from 'react-router-dom';
 
 export default function ShoppingCart(props) {
 
+    // props
+
     const { cart, setCart } = props;
 
     React.useEffect(() => {
         console.log("props in <ShoppingCart/ >", props.cart);
     });
 
+    // checkout "functionality" to ask for payment details
+
     const checkout = () => {
         prompt("Please enter your payment information");
     }
 
     return (
+
+        // hide cart link and renders cart + checkout button
+
         <div>
             <h2>Your Cart</h2>
             <nav>
